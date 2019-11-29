351ebc25ea51f1dfdaff779fb83f647c3bebe0e5
xeusteerapat <xeus085@gmail.com>
2019-11-29 23:51:19 +0700

    configure syntax highlighting

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,35 @@ module.exports = {
               linkImagesToOriginal: false,
             },
           },
+          "gatsby-remark-prismjs",
+          {
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+              languageExtensions: [
+                {
+                  language: "superscript",
+                  extend: "javascript",
+                  definition: {
+                    superscript_types: /(SuperType)/,
+                  },
+                  insertBefore: {
+                    function: {
+                      superscript_keywords: /(superif|superelse)/,
+                    },
+                  },
+                },
+              ],
+              prompt: {
+                user: "root",
+                host: "localhost",
+                global: false,
+              },
+            },
+          },
         ],
       },
     },
