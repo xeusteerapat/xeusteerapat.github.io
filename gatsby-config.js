5c03a28c2f573dd03f42c01df6366742eee0ea17
xeusteerapat <xeus085@gmail.com>
2020-04-03 21:51:36 +0700

    completed todo list blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,42 @@
 module.exports = {
-  pathPrefix: "",
+  pathPrefix: '',
   siteMetadata: {
-    title: "Hello, World",
-    author: "Teerapat Prommarak",
+    title: 'Hello, World',
+    author: 'Teerapat Prommarak',
     description:
-      "Full-stack web developer who always curiosity and learn new things.",
-    url: "https://xeusteerapat.github.io/",
-    twitterUsername: "@xeusteerapat",
+      'Full-stack web developer who always curiosity and learn new things.',
+    url: 'https://xeusteerapat.github.io/',
+    twitterUsername: '@xeusteerapat'
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sass",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "src",
-        path: `${__dirname}/src/`,
-      },
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
     },
-    "gatsby-plugin-sharp",
+    'gatsby-plugin-sharp',
     {
-      resolve: "gatsby-transformer-remark",
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
-          "gatsby-remark-relative-images",
+          'gatsby-remark-relative-images',
           {
-            resolve: "gatsby-remark-images",
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 750,
-              linkImagesToOriginal: false,
-            },
+              linkImagesToOriginal: false
+            }
           },
           {
             resolve: `gatsby-remark-vscode`,
             // All options are optional. Defaults shown here.
             options: {
-              theme: "Dark+ (default dark)", // Read on for list of included themes. Also accepts object and function forms.
-              wrapperClassName: "", // Additional class put on 'pre' tag. Also accepts function to set the class dynamically.
+              theme: 'Dark+ (default dark)', // Read on for list of included themes. Also accepts object and function forms.
+              wrapperClassName: '', // Additional class put on 'pre' tag. Also accepts function to set the class dynamically.
               injectStyles: true, // Injects (minimal) additional CSS for layout and scrolling
               extensions: [], // Third-party extensions providing additional themes and languages
               languageAliases: {}, // Map of custom/unknown language codes to standard/known language codes
@@ -46,13 +46,20 @@ module.exports = {
                 content, //   - the string content of the line
                 index, //   - the zero-based index of the line within the code fence
                 language, //   - the language specified for the code fence
-                meta, //   - any options set on the code fence alongside the language (more on this later)
-              }) => "",
-              logLevel: "warn", // Set to 'info' to debug if something looks wrong
-            },
+                meta //   - any options set on the code fence alongside the language (more on this later)
+              }) => '',
+              logLevel: 'warn' // Set to 'info' to debug if something looks wrong
+            }
           },
-        ],
-      },
-    },
-  ],
-}
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {
+              destinationDir: `path/to/dir`,
+              ignoreFileExtensions: []
+            }
+          }
+        ]
+      }
+    }
+  ]
+};
