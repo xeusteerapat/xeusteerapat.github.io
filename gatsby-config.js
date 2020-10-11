37fb79893198671b75b1b1bf0cf4ca38d9aa5fe0
xeusteerapat <xeus085@gmail.com>
2020-10-11 16:17:05 +0700

    [update] images configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,14 @@ module.exports = {
         path: `${__dirname}/src/`
       }
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`
+      }
+    },
+    'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-transformer-remark',
