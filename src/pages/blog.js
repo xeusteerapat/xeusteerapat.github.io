a00a1bf31ee115eeb19e80b15d4acc37b2a14324
xeusteerapat <xeus085@gmail.com>
2020-11-21 12:41:11 +0700

    [fix] key props in blogPage list

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -30,7 +30,7 @@ const BlogPage = () => {
       <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map(edge => {
           return (
-            <li className={blogStyles.post}>
+            <li className={blogStyles.post} key={edge.node.fields.slug}>
               <Link to={`/blog/${edge.node.fields.slug}`}>
                 <h2>{edge.node.frontmatter.title}</h2>
                 <p>{edge.node.frontmatter.date}</p>
