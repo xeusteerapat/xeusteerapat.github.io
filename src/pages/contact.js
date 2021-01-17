ad272f9d0660d6e1c7580838caf29ba101f8a3e8
xeusteerapat <xeus085@gmail.com>
2021-01-17 16:09:10 +0700

    [EDIT] update contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,13 +4,18 @@ import Head from '../components/head';
 import Twitter from '../../static/logo-twitter.svg';
 import Linkedin from '../../static/logo-linkedin.svg';
 import Github from '../../static/logo-github.svg';
+import Facebook from '../../static/facebook.svg';
 
 const ContactPage = () => {
   const logoStyle = {
     width: '30px',
     height: 'auto',
-    marginRight: '5px',
+    marginRight: '10px',
     marginBottom: '-5px'
+  };
+
+  const linkStyle = {
+    textDecoration: 'none'
   };
 
   return (
@@ -19,9 +24,21 @@ const ContactPage = () => {
       <h1>Contact</h1>
       <p>
         <a
+          href="https://www.facebook.com/curiousdevxeus"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={linkStyle}
+        >
+          <img src={Facebook} alt="Facebook" style={logoStyle} />
+          Curious Dev
+        </a>
+      </p>
+      <p>
+        <a
           href="https://twitter.com/xeusteerapat"
           target="_blank"
           rel="noopener noreferrer"
+          style={linkStyle}
         >
           <img src={Twitter} alt="Twitter" style={logoStyle} />
           xeusteerapat
@@ -32,6 +49,7 @@ const ContactPage = () => {
           href="https://github.com/xeusteerapat"
           target="_blank"
           rel="noopener noreferrer"
+          style={linkStyle}
         >
           <img src={Github} alt="Github" style={logoStyle} />
           Github
@@ -42,6 +60,7 @@ const ContactPage = () => {
           href="https://www.linkedin.com/in/teerapat-prommarak-44923697/"
           target="_blank"
           rel="noopener noreferrer"
+          style={linkStyle}
         >
           <img src={Linkedin} alt="Linkedin" style={logoStyle} />
           Linkedin
