e0171299762a4e0e4056315a8c15442f38b87c20
xeusteerapat <xeus085@gmail.com>
2022-01-30 10:46:09 +0700

    :wrench: [Update] configure pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,16 @@
 const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
 
-module.exports.onCreateNode = ({ node, actions }) => {
+module.exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
 
   if (node.internal.type === 'MarkdownRemark') {
-    const slug = path.basename(node.fileAbsolutePath, '.md');
+    const slug = createFilePath({ node, getNode });
 
     createNodeField({
       node,
       name: 'slug',
-      value: slug
+      value: slug,
     });
   }
 };
@@ -31,13 +32,36 @@ module.exports.createPages = async ({ graphql, actions }) => {
     }
   `);
 
-  res.data.allMarkdownRemark.edges.forEach(edge => {
+  if (res.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+
+  // Create blog-list pages
+  const posts = res.data.allMarkdownRemark.edges;
+  const postsPerPage = 6;
+  const numPages = Math.ceil(posts.length / postsPerPage);
+
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+      component: path.resolve('./src/templates/blogList.js'),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    });
+  });
+
+  res.data.allMarkdownRemark.edges.forEach((edge) => {
     createPage({
       component: blogTemplate,
       path: `/blog/${edge.node.fields.slug}`,
       context: {
-        slug: edge.node.fields.slug
-      }
+        slug: edge.node.fields.slug,
+      },
     });
   });
 };
